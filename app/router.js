8dfe32a84530641765f090f53a082f164f17c09b
monsteronfire <monsteronfire@gmail.com>
2016-09-12 13:18:32 +0800

    Add sub-routes under blog route

--- app/router.js
+++ app/router.js
@@ -7,7 +7,10 @@ const Router = Ember.Router.extend({
 });
 
 Router.map(function() {
-  this.route('blog');
+  this.route('blog', function() {
+    this.route('author');
+    this.route('edit');
+  });
 });
 
 export default Router;
